c9746d0cfe43e87bb5189f2a02a6107f67447410
RuoShui <ruoshui0014@outlook.com>
2025-06-02 11:49:55 +0800

    fix nodejs 22+ api update

--- Sites/rs_药监局.js
+++ Sites/rs_药监局.js
@@ -1,5 +1,4 @@
 debugger
-
 rsvm.siteInfo.version = "rs6_yaojian";
 rsvm.siteInfo.url = 'https://www.nmpa.gov.cn/datasearch/home-index.html?3jfdxVGGVXFo=1708324078947';
 rsvm.siteInfo.referrer = 'https://www.nmpa.gov.cn/datasearch/home-index.html?3jfdxVGGVXFo=1708324078947';
